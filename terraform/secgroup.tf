210ddaa17ae18057a1924e0e36b4028b8313d072
Your Name <you@example.com>
2018-10-06 00:49:30 -0400

    Added Jupyter port 8888 to security group

--- terraform/secgroup.tf
+++ terraform/secgroup.tf
@@ -9,6 +9,14 @@ resource "aws_security_group" "311_access" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    from_port   = 8888 
+    to_port     = 8888
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
   tags {
     Name = "instance_311"
   }
